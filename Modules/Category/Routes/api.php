6afac55455f635cafa66895efdc555f326f6f145
majidkm11 <majidkamali04@gmail.com>
2021-08-10 12:00:07 +0430

    add comment

--- Modules/Category/Routes/api.php
+++ Modules/Category/Routes/api.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-=======
-
->>>>>>> bb13c66c54c4224c40d235a12d93d026c44ae6d4
 /*
 |--------------------------------------------------------------------------
 | API Routes
