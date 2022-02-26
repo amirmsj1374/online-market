bbf7f87efbaef3ee28e9f414d8f2d31f384b27b1
TryCatch <majidkm2025@gmail.com>
2022-02-26 10:03:18 +0330

    add: hlper category class to category module

---
+++ Modules/Category/Http/Helper/Category.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\Category\Http\Helper;
+
+use AliBayat\LaravelCategorizable\Category as LaravelCategorizableCategory;
+use Illuminate\Support\Facades\Log;
+
+class Category extends LaravelCategorizableCategory
+{
+    public $appends = ['height'];
+
+    public function getHeightAttribute()
+    {
+
+        $height = 1;
+        if ($this->parent()->get()->isNotEmpty() && $this->getPrevSiblings()->isNotEmpty()) {
+            foreach ($this->getPrevSiblings() as $key => $prevSibling) {
+                $height += $this->descendantsAndSelf($prevSibling->id)->count();
+            }
+        }
+        return $height;
+    }
+}
