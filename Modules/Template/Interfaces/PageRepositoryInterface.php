07752690879bd90d95d90d4cf2a0cca84d158908
amirmsj1374 <amir.msj74@gmail.com>
2022-02-27 10:42:56 +0330

    Fix: problem in PageRepository Modify: ManagerControler getSectionsOfPage

--- Modules/Template/Interfaces/PageRepositoryInterface.php
+++ Modules/Template/Interfaces/PageRepositoryInterface.php
@@ -5,5 +5,5 @@ namespace Modules\Template\Interfaces;
 use Modules\Template\Entities\Page;
 
 interface PageRepositoryInterface {
-    public function find(Page $page);
+    public function find($pageId);
 }
